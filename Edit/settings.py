32e3a210e29bdff89fe28424f9a06a48cf6a77fe
Aaron KASONGO <19kk181@esisalama.org>
2022-02-17 10:29:23 +0100

    adding authentication

--- Edit/settings.py
+++ Edit/settings.py
@@ -113,6 +113,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Login url
+
+LOGIN_URL = 'login/'
+LOGIN_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- edit_app/views.py
+++ edit_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-
+@login_required
 def home(request):
     return render(request, 'edit_app/index.html')
+
+
+def signin(request):
+    return HttpResponse('Merci de vous connecter !')
