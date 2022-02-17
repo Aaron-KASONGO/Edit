5d7c42b9f216e4440419e9b3823e5b51597f0464
Aaron KASONGO <19kk181@esisalama.org>
2022-02-17 10:07:19 +0100

    Adding model crime

--- edit_app/models.py
+++ edit_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Crime(models.Model):
+    choices = [
+        ('VL', 'Vol'),
+        ('AG', 'Agression'),
+        ('CB', 'Cambrioloage')
+    ]
+
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=30)
+    category = models.CharField(max_length=2, choices=choices)
+    description = models.TextField()
+    date_crime = models.DateTimeField()
+    date_now = models.DateTimeField(auto_now_add=True)
+    long = models.CharField(max_length=15)
+    lat = models.CharField(max_length=15)
