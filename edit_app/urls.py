86ed5fdccfc9e1a511bfee7566e4f199b3eb98ae
Aaron KASONGO <19kk181@esisalama.org>
2022-02-20 20:57:09 +0100

    complete authentication process

--- edit_app/urls.py
+++ edit_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.signin, name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='edit_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('signup/', views.sign_up, name='signup'),
 ]
--- edit_app/views.py
+++ edit_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
@@ -11,3 +13,20 @@ def home(request):
 
 def signin(request):
     return HttpResponse('Merci de vous connecter !')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'edit_app/signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'edit_app/signup.html', {'form': form})
